adbf642dbdceb987b71783b5eee241841cfd79ec
kkrishna2503 <kkrishnas2503@gmail.com>
2026-09-12 12:18:34 +0530

    Update __init__.py

---
+++ src/explainability/__init__.py
@@ -0,0 +1,9 @@
+from .explanation import explain_candidate
+from .comparison import compare_candidates
+from .fairness import check_jd_fairness
+
+__all__ = [
+    "explain_candidate",
+    "compare_candidates",
+    "check_jd_fairness",
+]
